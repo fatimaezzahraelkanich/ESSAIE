9c04ef5a80ca5ca7f5cf165dd8b504733a40bbcc
fatimaezzahraelkanich <161979789+fatimaezzahraelkanich@users.noreply.github.com>
2024-04-15 00:18:36 +0100

    EX1

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class Main{
+    public static void main(String[] args) {
+        System.out.print("entrer le n:");
+        Scanner l = new Scanner(System.in);
+        int n = l.nextInt();
+        int T[]=new int[n];
+        float sum =0;
+        for(int i=0;i<n;i++){
+            System.out.println("entrez la valeur T["+(i+1)+"]");
+            T[i]=l.nextInt();
+            sum +=T[i];}
+        System.out.println("la somme est "+sum+"\nla mayenne est "+(sum/n));
+        int[] TT = Arrays.copyOf(T, n);
+        System.out.println("Tableau copié TT  : " + Arrays.toString(TT));
+        Arrays.sort(T);
+        System.out.println("Tableau trié par ordre : " + Arrays.toString(T));
+
+    }
+}
+
